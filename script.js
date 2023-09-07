be8bb78a7c062f543718e6c378a60ba1844ae533
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-07 19:10:10 -0400

    implemented logic to dynamically switch number inputs and display based on boolean trigger

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ let operandClicked = false;
 
 let displayText = document.getElementById("screen");
 let numbers = document.querySelectorAll(".numerical-button");
+let operands = document.querySelectorAll(".operand-button");
+
+operands.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    operandClicked = true;
+    console.log(operandClicked);
+    displayText.innerText = e.target.innerText;
+  });
+});
 
 numbers.forEach((button) => {
   button.addEventListener("click", (e) => {
-    numOne += e.target.value;
-    console.log(`NumOne: ${numOne}`);
-    console.log(typeof numOne);
-    displayText.innerText = numOne;
+    if (operandClicked === false) {
+      numOne += e.target.value;
+      console.log(`numOne: ${numOne}`);
+      console.log(typeof numOne);
+      displayText.innerText = numOne;
+    } else {
+      numTwo += e.target.value;
+      console.log(`numTwo: ${numTwo}`);
+      console.log(typeof numTwo);
+      displayText.innerText = numTwo;
+    }
   });
 });
 
