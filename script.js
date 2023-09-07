a5e175a29eb1fbde59bb781059d178fa3ebdcd64
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-07 18:54:33 -0400

    numerical buttons displaying on calculator as concat string when clicked

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let x = 0;
-let y = 0;
-let z = null;
+let numOne = "";
+let numTwo = "";
+let operandClicked = false;
+
+let displayText = document.getElementById("screen");
+let numbers = document.querySelectorAll(".numerical-button");
+
+numbers.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    numOne += e.target.value;
+    console.log(`NumOne: ${numOne}`);
+    console.log(typeof numOne);
+    displayText.innerText = numOne;
+  });
+});
 
 function add(inputOne, inputTwo) {
   return inputOne + inputTwo;
@@ -19,5 +31,5 @@ function divide(inputOne, inputTwo) {
 }
 
 function operate(numberOne, numberTwo, operator) {
-  return operator(numberOne, numberTwo);
+  operator(numberOne, numberTwo);
 }
