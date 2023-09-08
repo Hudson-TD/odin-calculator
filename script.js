3e47b886735f46044d43808d5a3e8fff4a2de48a
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-08 10:57:01 -0400

    HTML and CSS rework for a more traditional calculator look

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let numOne = "";
-let numTwo = "";
-let operandClicked = false;
+class Calculator {
+  constructor(prevNumberText, currentNumberText) {
+    this.prevNumberText = prevNumberText;
+    this.currentNumberText = currentNumberText;
+    this.clear();
+  }
+
+  clear() {
+    this.currentNumberText = '';
+    this.c
+  }
+}
 
 let displayText = document.getElementById("screen");
 const numbers = document.querySelectorAll(".numerical-button");
 const operands = document.querySelectorAll(".operand-button");
 const clearBtn = document.getElementById("clear-button");
+const submitBtn = document.getElementById("submit-button");
 
 operands.forEach((button) => {
   button.addEventListener("click", (e) => {
     operandClicked = true;
-    console.log(operandClicked);
-    displayText.innerText = e.target.innerText;
+    selectedOperand = e.target.value;
+    console.log(selectedOperand);
   });
 });
 
@@ -47,15 +57,35 @@ function divide(inputOne, inputTwo) {
   return inputOne / inputTwo;
 }
 
-function operate(numberOne, numberTwo, operator) {
-  operator(numberOne, numberTwo);
+function operate() {
+  if (selectedOperand === "add") {
+    let sum = add(Number(numOne), Number(numTwo));
+    let result = sum;
+    displayText.innerText = result;
+  } else if (selectedOperand === "subtract") {
+    let difference = subtract(Number(numOne), Number(numTwo));
+    let result = difference;
+    displayText.innerText = result;
+  } else if (selectedOperand === "multiply") {
+    let product = multiply(Number(numOne), Number(numTwo));
+    displayText.innerText = product;
+    let result = product;
+    displayText.innerText = result;
+  } else if (selectedOperand === "divide") {
+    let quotient = divide(Number(numOne), Number(numTwo));
+    displayText.innerText = quotient;
+    let result = quotient;
+    displayText.innerText = result;
+  }
 }
 
 function reset() {
   numOne = "";
   numTwo = "";
+  result = "";
   operandClicked = false;
-  displayText = "";
+  displayText.innerText = "";
 }
 
 clearBtn.addEventListener("click", reset);
+submitBtn.addEventListener("click", operate);
